86c29928bd6c4d6a888ca89739d31f2e067dc795
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-05 15:26:39 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
@@ -19,11 +21,13 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
         return NULL;
     }
     
-    i = 0;
-    while (i < n)
+    unsigned char *d = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
+    
+    for (i = 0; i < n; i++)
     {
-        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-        i++;
+        d[i] = s[i];
     }
+    
     return dest;
 }
