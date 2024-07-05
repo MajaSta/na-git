a617c5806cbccb34a50cc77961c5c8ec9a505ec0
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-05 15:34:26 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,12 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
+#include "libft.h"
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
     size_t i;
 
+    if (n == 0)
+    {
+        return dest;
+    }
+    
     if (!dest || !src)
     {
         return NULL;
