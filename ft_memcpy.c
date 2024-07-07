a841bf969382c02d2a8d50f4f596af1f6e749e14
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-07 18:47:43 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -35,3 +35,36 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
     
     return dest;
 }
+
+/*
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	unsigned char	*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	if (n == 0)
+	{
+		return (dest);
+	}
+	if (!dest || !src)
+	{
+		return (NULL);
+	}
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
+ft_memcpy.c: Error!
+Error: MISALIGNED_VAR_DECL  (line:  18, col:  21):	Misaligned variable declaration
+Error: MISALIGNED_VAR_DECL  (line:  19, col:  25):	Misaligned variable declaration
+
