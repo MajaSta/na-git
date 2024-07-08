018c70098b9d4484baeb513bd5a7bc8fcaa462d8
MajaSta <142170733+MajaSta@users.noreply.github.com>
2024-07-08 19:14:31 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,6 +9,36 @@
 /*   Updated: 2024/07/03 16:01:25 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+int ft_atoi(const char *str) {
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+
+    if (!str) {
+        return 0;
+    }
+    
+    // Skip whitespace
+    while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r')) {
+        i++;
+    }
+    
+    // Handle sign
+    if (str[i] == '-' || str[i] == '+') {
+        if (str[i] == '-') {
+            sign = -1;
+        }
+        i++;
+    }
+    
+    // Convert digits to integer
+    while (str[i] >= '0' && str[i] <= '9') {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    
+    return result * sign;
+}
 
 #include "libft.h"
 
